292dbb954b014e05f060c1dddb5047ce508c1966
Shubhi Singh <shubhisingh@gmail.com>
2026-01-06 19:25:45 +0530

    Docker config

--- src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -1,5 +1,7 @@
 package com.fitness.activityservice.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fitness.activityservice.dto.ActivityRequest;
@@ -16,18 +18,27 @@ public class ActivityService {
 	private final ActivityRepository activityRepository;
 
 	private final UserValidationService userValidationService;
+	private final KafkaTemplate<String, Activity> kafkaTemplate;
+	@Value("${kafka.topic.name}")
+	private String topicName;
 
 	public ActivityResponse trackActivity(ActivityRequest request) {
 
 		boolean isValidUser = userValidationService.validateUser(request.getUserId());
-		if(!isValidUser) {
-			throw new RuntimeException("Invalid User: "+ request.getUserId());
+		if (!isValidUser) {
+			throw new RuntimeException("Invalid User: " + request.getUserId());
 		}
 		Activity activity = Activity.builder().userId(request.getUserId()).type(request.getType())
 				.duration(request.getDuration()).caloriesBurned(request.getCaloriesBurned())
 				.startTime(request.getStartTime()).additionalMetrics(request.getAdditionalMetrics()).build();
 
 		Activity savedActivity = activityRepository.save(activity);
+		try {
+			kafkaTemplate.send(topicName, savedActivity.getUserId(), savedActivity);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 		return mapToResponse(savedActivity);
 	}
 
